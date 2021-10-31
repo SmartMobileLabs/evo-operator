66f0e386c46b8a73dcb1eb4615fe2e34fe0712c0
claus <claus.keuker@smartmobilelabs.com>
2021-10-31 23:54:28 +0100

    correct service account name

--- doit.sh
+++ doit.sh
@@ -1,8 +1,8 @@
- export VERSION=0.0.0.56
+ export VERSION=0.0.0.63
  microk8s kubectl delete -f config/samples/app.dac.nokia.com_v1alpha1_smlevo.yaml -n sml-evo
  make docker-build  docker-push deploy
  sleep 20
- kubectl create ns sml-evo
+ microk8s kubectl create ns sml-evo
  microk8s kubectl apply -f config/samples/app.dac.nokia.com_v1alpha1_smlevo.yaml  -n sml-evo
 
 
