4d087209a5bd77a8195bc16d77f8145d20d5e257
claus <claus.keuker@smartmobilelabs.com>
2021-11-01 11:45:19 +0100

    some stuff

--- doit.sh
+++ doit.sh
@@ -1,8 +1,8 @@
- export VERSION=0.0.0.64
- microk8s kubectl delete -f config/samples/app.dac.nokia.com_v1alpha1_smlevo.yaml -n sml-evo
- make docker-build  docker-push deploy
- sleep 20
+ export VERSION=0.0.66
+# microk8s kubectl delete -f config/samples/app.dac.nokia.com_v1alpha1_smlevo.yaml -n sml-evo
+ make docker-build  docker-push bundle
  microk8s kubectl create ns sml-evo
- microk8s kubectl apply -f config/samples/app.dac.nokia.com_v1alpha1_smlevo.yaml  -n sml-evo
+ microk8s kubectl create ns sml-evo-system
+# microk8s kubectl apply -f config/samples/app.dac.nokia.com_v1alpha1_smlevo.yaml  -n sml-evo
 
 
