cd4ae7fbbb58f8cc3ef965e8cdabffa516102628
claus <claus.keuker@smartmobilelabs.com>
2021-11-17 18:24:53 +0100

    fixing different stuff

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,7 @@
 	kubectl -n sml-evo delete smlevoes.app.dac.nokia.com  sml-evo-1
 	kubectl -n sml-evo patch smlevoes.app.dac.nokia.com sml-evo-1 -p '{"metadata": {"finalizers": null}}' --type=merge
 	# kubectl -n sml-evo patch resourcerequest.ops.dac.nokia.com/resource-for-sml-evo -p '{"metadata": {"finalizers": null}}' --type=merge
+	# kubectl -n sml-evo patch storage.ops.dac.nokia.com/storage-persvol-1 -p '{"metadata": {"finalizers": null}}' --type=merge
 	kubectl -n sml-evo get smlevoes.app.dac.nokia.com
 	kubectl delete ns sml-evo
 	kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 microk8s kubectl get --show-kind --ignore-not-found -A | grep sml
--- controllers/handlers.go
+++ controllers/handlers.go
@@ -198,7 +198,7 @@ func (r *SmlEvoReconciler) handleCreate(instance *app.SmlEvo, namespace string) 
 		func() {
 			logger.Info("Set AppReportedData")
 			//runningCallback - example, some dynamic data should be reported here which has value only after the deployment
-			svc, err := kubelib.GetKubeAPI().CoreV1().Services(namespace).Get(context.TODO(), "sml-evo-operator-metrics", metav1.GetOptions{})
+			svc, err := kubelib.GetKubeAPI().CoreV1().Services(namespace).Get(context.TODO(), "sml-evo-service", metav1.GetOptions{})
 			if err != nil {
 				logger.Error(err, "Failed to read the svc of the metrics endpoint")
 				return
